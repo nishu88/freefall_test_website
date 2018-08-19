23f05eebd8da0fbe7e814b0ea69a8e3c2d657a50
Nishanth D A <nishanthdilip@yahoo.co.in>
2018-08-19 14:40:25 +0530

    changes

--- app.py
+++ app.py
@@ -84,7 +84,11 @@ def test_disconnect():
 
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()    
     app.run(host='0.0.0.0', port=5000)
     socketio.run(app)
    
